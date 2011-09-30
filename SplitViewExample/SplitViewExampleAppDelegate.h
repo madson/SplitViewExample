1b663dbc6c186c778a3f44223ae3d865edd4fc53
Madson Cardoso <madsonmac@gmail.com>
2011-09-30 00:36:25 -0300

    Initial Commit

---
+++ SplitViewExample/SplitViewExampleAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SplitViewExampleAppDelegate.h
+//  SplitViewExample
+//
+//  Created by Madson on 30/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SplitViewExampleAppDelegate : NSObject <UIApplicationDelegate>
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
