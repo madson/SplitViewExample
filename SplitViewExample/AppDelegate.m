6a8f0a63d7ca0535d56c6b6c0127234f9d5f1565
Madson Cardoso <madsonmac@gmail.com>
2011-10-05 00:16:13 -0300

    Renamed the application delegate class

---
+++ SplitViewExample/AppDelegate.m
@@ -0,0 +1,46 @@
+//
+//  SplitViewExampleAppDelegate.m
+//  SplitViewExample
+//
+//  Created by Madson on 30/09/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+#import "MenuViewController.h"
+#import "DetailViewController.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+    MenuViewController *menu        = [MenuViewController new];
+    UINavigationController *nav     = [[UINavigationController alloc] initWithRootViewController:menu];
+    
+    DetailViewController *detail    = [DetailViewController new];
+    menu.detailController           = detail;
+    
+    UISplitViewController *split    = [UISplitViewController new];
+    split.viewControllers           = [NSArray arrayWithObjects:nav, detail, nil];
+    split.delegate                  = detail;
+    [detail release];
+    
+    [self.window setRootViewController:split];
+    [self.window makeKeyAndVisible];
+
+    [menu release];
+    [nav release];
+    [split release];
+    
+    return YES;
+}
+
+- (void)dealloc
+{
+    [_window release];
+    [super dealloc];
+}
+
+@end
